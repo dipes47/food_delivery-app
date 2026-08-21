748dbe76635b0eee0eb542a4fe5bc7eb870eec90
jane <dipes94@gmail.com>
2026-08-21 15:52:17 +0530

    latest bug fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(
+      "mongodb+srv://workdipesh17_db_user:UypJDVCzUVals9OK@cluster0.dxnswtl.mongodb.net/"
+    )
     .then(() => console.log("DB Connected"));
 };
 
