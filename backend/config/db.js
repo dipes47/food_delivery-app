37a9c48a0ba2430fa76bc94b96744864b72dd9fa
jane <dipes94@gmail.com>
2026-08-21 16:01:30 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://workdipesh17_db_user:UypJDVCzUVals9OK@cluster0.dxnswtl.mongodb.net/"
-    )
+    .connect("Your MONGO_DB_URL")
     .then(() => console.log("DB Connected"));
 };
-
 // add your mongoDB connection string above.
 // Do not use '@' symbol in your database user's password else it will show an error.
